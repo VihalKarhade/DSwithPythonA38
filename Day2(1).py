562c31ecd3d527a422370ab60bc6956df75aac59
VihalKarhade <130913527+VihalKarhade@users.noreply.github.com>
2023-07-12 14:00:38 +0530

    Add files via upload

---
+++ Day2(1).py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=60.2
+print(a)
+
+
+# In[2]:
+
+
+a
+
+
+# In[3]:
+
+
+type(a)
+
+
+# In[4]:
+
+
+print('a')
+
+
+# In[5]:
+
+
+a=30
+b=20
+
+
+# In[9]:
+
+
+print(a,b)
+
+
+# In[10]:
+
+
+a==30
+
+
+# In[12]:
+
+
+b==20
+
+
+# In[13]:
+
+
+b==30
+
+
+# In[14]:
+
+
+a!=20
+
+
+# In[15]:
+
+
+a=get_ipython().getoutput('20')
+
+
+# In[16]:
+
+
+a=30
+a!=30
+
+
+# In[18]:
+
+
+a='Vihal'
+
+
+# In[19]:
+
+
+len(a)
+
+
+# In[20]:
+
+
+a='Vihal'
+b='Karhade'
+c=a+b
+
+
+# In[21]:
+
+
+print(c)
+
+
+# In[22]:
+
+
+len(c)
+
+
+# In[23]:
+
+
+type(c)
+
+
+# In[24]:
+
+
+a='Ashish@2748'
+type(a)
+
+
+# In[25]:
+
+
+a=2748
+type(a)
+
+
+# In[26]:
+
+
+a=@
+type(a)
+
+
+# In[27]:
+
+
+#arithmatic operator
+a=20
+b=30
+c=a+b
+
+
+# In[28]:
+
+
+c
+
+
+# In[29]:
+
+
+d=b-a
+
+
+# In[30]:
+
+
+print(b)
+
+
+# In[31]:
+
+
+e=b%a
+print(e)
+
+
+# In[33]:
+
+
+f=b/a
+print(f)
+
+
+# In[34]:
+
+
+g=a*b
+
+
+# In[35]:
+
+
+print(g)
+
+
+# In[1]:
+
+
+a=True
+b=False
+#logical operator
+
+
+# In[2]:
+
+
+a&b
+
+
+# In[3]:
+
+
+a|b
+
+
+# In[ ]:
+
+
+
+
