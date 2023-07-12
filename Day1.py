2033a9c6e0e506f55b6e0db66c58714b1b0b34b8
VihalKarhade <130913527+VihalKarhade@users.noreply.github.com>
2023-07-12 10:25:49 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=20
+#a=variable
+#assignment operator
+#20=data
+
+
+# In[2]:
+
+
+a
+
+
+# In[4]:
+
+
+print(a)
+
+
+# In[8]:
+
+
+a="Vihal"
+a='Vihal'
+a='''Vihal'''
+#Vihal= String
+
+
+# In[9]:
+
+
+a
+
+
+# In[10]:
+
+
+a='Vihal'
+b='Karhade'
+c=a+b
+#concatenation
+
+
+# In[11]:
+
+
+c
+
+
+# In[12]:
+
+
+#Escape letter
+#\n=new line character
+print("Vihal\nKarhade")
+
+
+# In[13]:
+
+
+#\t= tab
+print('Vihal\tKarhade')
+
+
+# In[14]:
+
+
+a=20
+
+
+# In[16]:
+
+
+type(a)
+
+
+# In[17]:
+
+
+a='Vihal'
+
+
+# In[18]:
+
+
+type(a)
+
+
+# In[ ]:
+
+
+
+
